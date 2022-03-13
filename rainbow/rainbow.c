9ece2b15cea222af1b6faa9fe9130c2421bf29b5
llwyd <tom@llwyd.io>
2022-03-13 16:22:51 +0000

    new util module and tidied up init code

--- rainbow/rainbow.c
+++ rainbow/rainbow.c
@@ -4,6 +4,7 @@
 
 /* ATSAMD21E18 */
 
+#include "../common/util.h"
 
 /* Registers for GPIO Config */
 #define PORT        ( *( ( volatile unsigned int *)0x41004400 ) )
@@ -15,44 +16,53 @@
 #define STK_VAL     ( *( ( volatile unsigned int *)0xE000E018 ) )
 #define STK_CALIB   ( *( ( volatile unsigned int *)0xE000E01C ) )
 
+/* SysCtrl */
+#define SYSCTRL_8MHZ ( *( ( volatile unsigned int *)0x40000820 ) )
+
 #define LED_PIN ( 10U )
 
 /* SysTick ISR */
 void _sysTick( void )
 {
     /* XOR Toggle of On-board LED */
-    PIN ^= ( 1 << LED_PIN );
+    TOG( PIN, 0x1, LED_PIN );
+}
+
+static void Init( void )
+{
+    /* 8Mhz Clock */
+    CLR( SYSCTRL_8MHZ, 0x3, 8U );
+    
+    /* set port 10 to output */
+    SET( PORT, 1, LED_PIN );
+    SET( PIN, 1, LED_PIN );
+
+    /* Reset SysTick Counter and COUNTFLAG */
+    STK_VAL = 0x0;
+    /* SysTick Calibration value for 10ms tick as per ARM datasheet
+     * 8MHz Clock / 100Hz tick = 0x13880
+     * Need to subtract 1 because count ends at 0 so
+     * Calibration value is 0x1387F
+     */
+    STK_CALIB = ( 0x1387F );
+    
+    /* 500ms Blink is previous value * 50 */
+    STK_LOAD   = 0x1387F * 50;
+     
+    /* Enable SysTick interrupt, counter 
+     * and set processor clock as source */
+    SET( STK_CTRL, 0x7, 0U);
+    
+    /* Globally Enable Interrupts */
+    asm("CPSIE IF");
 }
 
 int main ( void )
 {
-    /* set port 10 to output */
-    PORT |= ( 1 << LED_PIN );
-    PIN |= ( 1 << LED_PIN );
- 
-    /* Reset SysTick Counter and COUNTFLAG */
-    STK_VAL = 0x0;
-
-    /* SysTick Calibration value for 10ms tick as per ARM datasheet
-     * 1MHz Clock / 100Hz tick = 0x2710
-     * Need to subtract 1 because count ends at 0 so
-     * Calibration value is 0x270F
-     */
-    STK_CALIB = ( 0x270F );
-    
-    /* 500ms Blink is previous value * 50 */
-    STK_LOAD   = 0x270F * 50;
-     
-    /* Enable SysTick interrupt, counter 
-     * and set processor clock as source */
-    STK_CTRL |= 0x7;
-
-    /* Globally Enable Interrupts */
-    asm("CPSIE IF");
+    Init();    
 
     /* Endless Loop */
     while(1);
-
     return 0;
 }
 
