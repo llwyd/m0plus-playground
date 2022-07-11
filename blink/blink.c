678b314302594cb74412bf19758d34813c7dc3a0
llwyd <tom@llwyd.io>
2022-07-11 21:19:30 +0100

    tidying up

--- blink/blink.c
+++ blink/blink.c
@@ -1,4 +1,4 @@
-/* A very minimalist blinky program for the Trinket m0+
+/* A template blinky program for the Trinket m0+
  * T.L. 2022
  * */
 
@@ -7,12 +7,6 @@
 #include "../common/fsm.h"
 #include "../common/util.h"
 
-/* SysTick registers */
-#define STK_CTRL    ( *( ( volatile unsigned int *)0xE000E010 ) )
-#define STK_LOAD    ( *( ( volatile unsigned int *)0xE000E014 ) )
-#define STK_VAL     ( *( ( volatile unsigned int *)0xE000E018 ) )
-#define STK_CALIB   ( *( ( volatile unsigned int *)0xE000E01C ) )
-
 #define LED_PIN ( 10U )
 
 enum Signals
@@ -20,13 +14,14 @@ enum Signals
     signal_SysTick = signal_Count,
 };
 
+static systick_t * SYSTICK = ( systick_t * ) SYSTICK_BASE;
 static gpio_t * GPIO = ( gpio_t * ) GPIO_BASE;
-static fsm_events_t events;
+static fsm_events_t * event_ptr;
 
 /* SysTick ISR */
 void _sysTick( void )
 {
-    FSM_AddEvent( &events, signal_SysTick );
+    FSM_AddEvent( event_ptr, signal_SysTick );
 }
 
 
@@ -60,6 +55,8 @@ static void Loop( void )
     fsm_t state;
     state.state = &Idle;
     signal sig = signal_None;
+    fsm_events_t events;
+    event_ptr = &events;
 
     FSM_Init( &state, &events );
 
@@ -78,21 +75,21 @@ static void Init( void )
     GPIO->OUT |= ( 1 << LED_PIN );
  
     /* Reset SysTick Counter and COUNTFLAG */
-    STK_VAL = 0x0;
+    SYSTICK->VAL = 0x0;
 
     /* SysTick Calibration value for 10ms tick as per ARM datasheet
      * 1MHz Clock / 100Hz tick = 0x2710
      * Need to subtract 1 because count ends at 0 so
      * Calibration value is 0x270F
      */
-    STK_CALIB = ( 0x270F );
+    SYSTICK->CALIB = ( 0x270F );
     
     /* 500ms Blink is previous value * 50 */
-    STK_LOAD   = 0x270F * 50;
+    SYSTICK->LOAD   = 0x270F * 50;
      
     /* Enable SysTick interrupt, counter 
      * and set processor clock as source */
-    STK_CTRL |= 0x7;
+    SYSTICK->CTRL |= 0x7;
 
     /* Globally Enable Interrupts */
     asm("CPSIE IF");
