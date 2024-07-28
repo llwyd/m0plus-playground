82f54ad88875c6a847fb620805c261e466ddafb5
llwyd <tom@llwyd.io>
2024-07-28 16:33:49 +0100

    random number module and example program

--- rng/rng.c
+++ rng/rng.c
@@ -1,12 +1,14 @@
 #include "gpio.h"
 #include "util.h"
 #include "systick.h"
+#include "random.h"
 #include <stdint.h>
 #include <stdbool.h>
 
 #define LED_PIN ( 5U )
 
 static volatile uint32_t no_init_data __attribute__((section(".no_init")));
+static random_t random_seed __attribute__((section(".no_init")));
 
 static void Loop( void )
 {
@@ -31,6 +33,7 @@ static void Init( void )
     
     GPIO_Init();
     GPIO_SetOutput(LED_PIN);
+    Random_Init(&random_seed);
 
     no_init_data = 0x12345678;
     /* Globally Enable Interrupts */
