af183bfa20aed72521d14049cecc93fffa08f331
llwyd <tom@llwyd.io>
2025-05-30 12:37:48 +0100

    project for testing pqs

---
+++ pq/pq.c
@@ -0,0 +1,20 @@
+/* Conway's game of life for the Trinket m0+ and a small lcd display
+ * T.L. 2022
+ * */
+
+/* ATSAMD21E18 */
+
+#include "util.h"
+#include "systick.h"
+#include <stdbool.h>
+
+
+int main ( void )
+{
+    while(1)
+    {
+
+    }
+    return 0;
+}
+
